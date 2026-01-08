3500eb6218f1aa5129d3c99020f97cf99c78ca43
rohit <rohitmahto062@gmail.com>
2026-01-08 17:29:21 +0530

    Add admin panel components and routing for blog management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import ListBlog from "./pages/admin/ListBlog";
+import AddBlog from "./pages/admin/AddBlog";
+import Comments from "./pages/admin/Comments";
 
 function App() {
   return (
@@ -9,6 +14,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/admin/listblog" element={<ListBlog />} />
+          <Route path="/admin/addblog" element={<AddBlog />} />
+          <Route path="/admin/comments" element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ client/src/pages/admin/Layout.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+import { Outlet, useNavigate } from "react-router-dom";
+
+const Layout = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      {/* navbar */}
+      <div className="flex justify-between items-center py-5 h-[70px] mx-8 sm:mx-20 xl:mx-32">
+        <img
+          src={assets.logo}
+          alt="logo"
+          className="w-32 sm:w-44 cursor-pointer"
+          onClick={() => navigate("/")}
+        />
+        <button
+          onClick={() => navigate("/")}
+          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-8 py-2.5 sm:px-10"
+        >
+          Logout
+          <img src={assets.arrow} alt="arrow" className="w-3" />
+        </button>
+      </div>
+
+      {/* content */}
+      <div className="flex h-[calc(100vh-70px)]">
+        <div>sidebar</div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
