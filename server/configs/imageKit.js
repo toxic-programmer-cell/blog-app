74f74eac5faeb62f44a9816de0eb26b74b2b92f6
rohit <rohitmahto062@gmail.com>
2026-01-24 00:49:18 +0530

    feat: add ImageKit integration for image management

---
+++ server/configs/imageKit.js
@@ -0,0 +1,9 @@
+import ImageKit from "@imagekit/nodejs";
+
+let imageKit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_ENDPOINT_URL,
+});
+
+export default imageKit;
